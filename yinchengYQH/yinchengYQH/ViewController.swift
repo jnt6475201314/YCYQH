0890989eccb70f41adf168fb56669262ad0d6942
374692767@qq.com <374692767@qq.com>
2019-08-14 17:29:48 +0800

    初始化项目

---
+++ yinchengYQH/yinchengYQH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yinchengYQH
+//
+//  Created by jnt on 2019/8/14.
+//  Copyright © 2019 qirong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
